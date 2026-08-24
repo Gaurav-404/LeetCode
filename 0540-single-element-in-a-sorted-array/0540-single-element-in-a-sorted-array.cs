fa4d2cbd4ef0891cecf63033781778cafcc9b7ed
Gaurav Singh Chandel <152051276+Gaurav-404@users.noreply.github.com>
2026-08-24 23:21:08 +0530

    Time: 0 ms (100%), Space: 50.3 MB (43.72%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
@@ -1,20 +1,40 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
         int start=0;
+        int n=nums.Length;
         int end=nums.Length-1;
-        while(end>start){
+        if(nums.Length==1){
+            return nums[0];
+        }
+        while(end>=start){
             int mid=start+(end-start)/2;
-            if(mid%2==1){
-                mid--;
+            if(mid==0 && nums[0]!=nums[1]){
+                return nums[0];
             }
-            if(nums[mid]==nums[mid+1]){
-                start=mid+2;
+            if(mid==n-1 && nums[n-1]!=nums[n-2]){
+                return nums[n-1];
+            }
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            if(mid%2==0){
+                if(nums[mid]==nums[mid-1]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
             }
             else{
-                end=mid;
+                if(nums[mid]==nums[mid-1]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
             }
         }
-        return nums[start];
+        return -1;
 
     }
 }
