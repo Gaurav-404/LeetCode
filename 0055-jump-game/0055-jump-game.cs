a59bfbc5219bbe8d2e4cc41aedb15463b9c9a998
Gaurav Singh Chandel <152051276+Gaurav-404@users.noreply.github.com>
2026-09-08 00:06:27 +0530

    Time: 2 ms (55.01%), Space: 57.6 MB (86.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.cs
+++ 0055-jump-game/0055-jump-game.cs
@@ -1,15 +1,13 @@
 public class Solution {
     public bool CanJump(int[] nums) 
     {
-        int finishIndex = nums.Length - 1;
-        for (int i = nums.Length - 1; i >= 0; i--)
-        {
-            if (i + nums[i] >= finishIndex)
-            {
-                if (i == 0) return true;
-                finishIndex = i;
+        int maxReach=0;
+        for(int i=0;i<nums.Length;i++){
+            if(i>maxReach){
+                return false;
             }
+            maxReach=Math.Max(maxReach,i+nums[i]);
         }
-        return false;
+        return true;
     }
 }
