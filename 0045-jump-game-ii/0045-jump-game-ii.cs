1ec2dd63f61d7385f26c09c02eb701b0cfb35924
Gaurav Singh Chandel <152051276+Gaurav-404@users.noreply.github.com>
2026-09-08 00:25:32 +0530

    Time: 0 ms (100%), Space: 45.5 MB (93.49%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int Jump(int[] nums) {
+        int currentEnd=0;
+        int jumps=0;
+        int farthest=0;
+        for(int i=0;i<nums.Length-1;i++){
+            farthest=Math.Max(farthest,i+nums[i]);
+            if(i==currentEnd){
+                jumps++;
+                currentEnd=farthest;
+            }
+        }
+        return jumps;
+    }
+}
