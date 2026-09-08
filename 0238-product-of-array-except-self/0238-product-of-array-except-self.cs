ca293321055369ea279ca2714374d2ef3a6c5099
Gaurav Singh Chandel <152051276+Gaurav-404@users.noreply.github.com>
2026-09-08 23:11:52 +0530

    Time: 1 ms (100%), Space: 62.7 MB (80.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -5,8 +5,8 @@ public class Solution {
         int suffix=1;
         ans[0]=1;
         for(int i=1;i<nums.Length;i++){
-            ans[i]=nums[i-1]*prefix;
-            prefix=ans[i];
+            ans[i]=prefix*nums[i-1];
+            prefix*=nums[i-1];
         }
         for(int i=nums.Length-2;i>=0;i--){
             ans[i]*=suffix*nums[i+1];
