89561aba55fb803e7bbe1729cc348a6206ee2381
Gaurav Singh Chandel <152051276+Gaurav-404@users.noreply.github.com>
2026-09-21 23:30:13 +0530

    Time: 1 ms (100%), Space: 61.2 MB (97.39%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int maxSum=int.MinValue;
+        int currSum=0;
+        for(int i=0;i<nums.Length;i++){
+            currSum+=nums[i];
+            maxSum=Math.Max(currSum,maxSum);
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
+    }
+}
