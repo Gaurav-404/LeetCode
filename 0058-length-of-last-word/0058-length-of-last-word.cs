914ce88599fadef761e67b71f9fe243b32e2a87a
Gaurav Singh Chandel <152051276+Gaurav-404@users.noreply.github.com>
2026-09-21 22:59:50 +0530

    Time: 0 ms (100%), Space: 40.4 MB (18%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cs
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -1,9 +1,6 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
-        s=s.Trim();
-        int n=s.Length;
-        int m=s.LastIndexOf(" ");
-        String r=s.Substring(m+1);
-        return r.Length;
+        string lastWord = s.Split(' ',StringSplitOptions.RemoveEmptyEntries).Last();
+        return lastWord.Length;
     }
 }
